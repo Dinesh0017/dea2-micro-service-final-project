307c52d7efb1c5e9cbd1b1f36248418aec533057
nipun-dezoysa <nipunavishka123@gmail.com>
2026-02-26 23:27:08 +0530

    Add eureka and gateway

--- backend/Gateway/src/main/java/com/wms/gateway/GatewayApplication.java
+++ backend/Gateway/src/main/java/com/wms/gateway/GatewayApplication.java
@@ -1,19 +1,16 @@
-package com.wms.workforce_equipment_service;
+package com.wms.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-/**
- * Main application class for the Workforce & Equipment Service.
- */
 @SpringBootApplication
 @EnableDiscoveryClient
-public class WorkforceEquipmentServiceApplication {
+public class GatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WorkforceEquipmentServiceApplication.class, args);
+		SpringApplication.run(GatewayApplication.class, args);
 	}
 
 }
