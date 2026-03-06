3b00471e1e2c4c78d504b8fea0bd06f9bc1cde93
Dilshan Sanjeewa Deshapriya <dilshansanjeewadeshappriya@gmail.com>
2026-03-06 22:20:32 +0530

    add test files

--- backend/Inventory Management Service/src/test/java/com/wms/inventory_management_service/InventoryManagementServiceApplicationTests.java
+++ backend/Inventory Management Service/src/test/java/com/wms/inventory_management_service/InventoryManagementServiceApplicationTests.java
@@ -1,9 +1,11 @@
 package com.wms.inventory_management_service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class InventoryManagementServiceApplicationTests {
 
 	@Test
