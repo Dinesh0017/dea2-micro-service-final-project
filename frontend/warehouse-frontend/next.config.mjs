298ff840a946e9570bc652d29c46b6f72116720c
Dilshan Sanjeewa Deshapriya <dilshansanjeewadeshappriya@gmail.com>
2026-03-06 23:18:55 +0530

    fix config issue

--- frontend/warehouse-frontend/next.config.mjs
+++ frontend/warehouse-frontend/next.config.mjs
@@ -1,7 +1,15 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  turbopack: {
+    root: __dirname,
+  },
   async rewrites() {
     const gateway =
       process.env.NEXT_PUBLIC_API_GATEWAY_URL || "http://131.163.97.60:8222";
